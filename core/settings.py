8c29601b4d2bddb6f1bae1219d2f1922482dca89
rishabhdev2700 <rishavkumar2700@gmail.com>
2025-06-06 18:19:51 +0530

    log

--- core/settings.py
+++ core/settings.py
@@ -136,7 +136,7 @@ else:
             "PORT": os.getenv("PGPORT"),
         }
     }
-
+print(DATABASES)
 AUTH_USER_MODEL = "accounts.User"
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
